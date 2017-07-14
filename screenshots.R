fb7a3dad9d8863af1d37879108d447b66ca6a15f
Are Strom <are.strom@gmail.com>
2017-07-13 18:13:46 -0700

    More blather

--- screenshots.R
+++ screenshots.R
@@ -7,3 +7,8 @@ library(dplyr)
 webshot("https://wdfw.iformbuilder.com/exzact/FormBuilder/pages/3358331", 
         "screenshots/actions.png", selector = ".actions", zoom = 25) %>% 
         shrink()
+
+# Get the whole page
+webshot("https://wdfw.iformbuilder.com/exzact/FormBuilder/pages/3358331", 
+        "screenshots/device.png", selector = ".z-index-fix", zoom = 50) %>% 
+  shrink()
